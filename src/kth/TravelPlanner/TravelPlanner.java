92446baa37491c8522d8cd616cca7fcbab47c02c
Joakim Candefors <m4ngokid@googlemail.com>
2012-04-16 01:07:44 +0200

    Main Class skapad

---
+++ src/kth/TravelPlanner/TravelPlanner.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package kth.TravelPlanner;
+
+/**
+ * @author j
+ *
+ */
+public class TravelPlanner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
