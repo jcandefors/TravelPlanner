9c1c822bfd03f5dc5fc8e3ff8de4ab6a602e888d
Joakim Candefors <m4ngokid@googlemail.com>
2012-04-16 01:21:16 +0200

    changed package name

---
+++ src/travelPlanner/TravelPlanner.java
@@ -0,0 +1,19 @@
+package travelPlanner;
+
+import javax.swing.JFrame;
+
+/**
+ * @author j
+ *
+ */
+public class TravelPlanner extends JFrame{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
