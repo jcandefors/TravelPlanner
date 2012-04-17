0dd51e4b3a33e9fc74fb2fdbde13a4283b483e1b
Joakim Candefors <m4ngokid@googlemail.com>
2012-04-17 17:21:20 +0200

    Push to master

--- src/travelPlanner/Slide.java
+++ src/travelPlanner/Slide.java
@@ -2,6 +2,30 @@ package travelPlanner;
 
 public class Slide {
 	
-	protected String[] data;
+protected String title;
+protected JFrame frame;
+protected String[] Data;
+bufferedReader fileReader;
+protected File aboutFile = filepath/about.txt - textfilen att med all data.
+protected File indexFile = filepath/index.txt - textfilen för att se "barn".
+protected String filepath;
+//(MapPanel map;)
 
+public Slide(String title)
+aboutFile = new File()
+loadDataFromFile();
+listSubclasses();
+
+listSubclass(); 
+
+	
+
+
+}
+private void listSubclasses() {
+	Listar projekt i portfolion, resmål i projektet från index.txt och returnerar i en String[].
+}
+private void loadDataFromFile() {
+	//Läser filen about och returnerar en string[] med informationen.
+	
 }
