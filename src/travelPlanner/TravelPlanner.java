f2e1aa11d764f989624572ac33c68c0c3361afa7
Joakim Candefors <m4ngokid@googlemail.com>
2012-04-18 19:17:51 +0200

    tagit bort och lagt till

---
+++ src/travelPlanner/TravelPlanner.java
@@ -0,0 +1,22 @@
+package travelPlanner;
+
+import javax.swing.JFrame;
+
+/**
+ * This is the main class of the application TravelPlanner
+ * @author j
+ *
+ */
+public class TravelPlanner extends JFrame{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// skapa frame
+		// skapa login
+		// login.login()
+
+	}
+
+}
