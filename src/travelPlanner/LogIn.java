9233ee5562351c0e3628b7e401018771ab7fb8f6
Ragnhild <ragnhild.karlsson@gmail.com>
2012-04-18 16:38:50 +0200

    created logIn classes

---
+++ src/travelPlanner/LogIn.java
@@ -0,0 +1,30 @@
+package travelPlanner;
+import java.awt.*;
+import java.awt.event*;
+import javax.swing.*;
+
+
+/**
+ * This class will start a pop-Up window where it is possible to 
+ * login to a program by typing in a username and a password that will be matched 
+ * against the user register for the program.
+ * The pop-Up window also contain a LINK to creating new users. 
+ * 
+ * @author ragnhild
+ * @version 0.1
+ *
+ */
+
+public class LogIn {
+
+private LogInWindow window;
+
+	public void login(){
+		
+	window = new LogInWindow();
+	window.
+		
+		
+		
+	}
+}
--- src/travelPlanner/User.java
+++ src/travelPlanner/User.java
@@ -2,4 +2,13 @@ package travelPlanner;
 
 public class User {
 
+	private final String username;
+	private String hashpassword;
+	
+	public User(String username){
+		this.username = username;	
+	}
+public String getUserName(){
+	return 
+}
 }
