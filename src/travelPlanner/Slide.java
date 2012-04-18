ddd777d0ec037379ef8c191bab632127166b2548
Joakim Candefors <m4ngokid@googlemail.com>
2012-04-18 16:25:01 +0200

    efter merge

--- src/travelPlanner/Slide.java
+++ src/travelPlanner/Slide.java
@@ -11,34 +11,6 @@ import java.util.ArrayList;
 
 
 public class Slide {
-<<<<<<< HEAD
-	
-protected String title;
-protected JFrame frame;
-protected String[] Data;
-bufferedReader fileReader;
-protected File aboutFile = filepath/about.txt - textfilen att med all data.
-protected File indexFile = filepath/index.txt - textfilen för att se "barn".
-protected String filepath;
-//(MapPanel map;)
-
-public Slide(String title)
-aboutFile = new File()
-loadDataFromFile();
-listSubclasses();
-
-listSubclass(); 
-
-	
-
-
-}
-private void listSubclasses() {
-	Listar projekt i portfolion, resmål i projektet från index.txt och returnerar i en String[].
-}
-private void loadDataFromFile() {
-	//Läser filen about och returnerar en string[] med informationen.
-=======
 
 	protected String title;
 	protected LayoutHandler layoutHandler;
@@ -92,6 +64,5 @@ private void loadDataFromFile() {
 		}
 		return null;
 	}
->>>>>>> TravelPlanner
 	
 }
