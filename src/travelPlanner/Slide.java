b57a4945af400b301d567220d35c65f2c276fb60
Joakim Candefors <m4ngokid@googlemail.com>
2012-04-23 23:29:28 +0200

    fixed destinationbutton in slide

--- src/travelPlanner/Slide.java
+++ src/travelPlanner/Slide.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
@@ -100,7 +101,7 @@ public class Slide {
 	/**
 	 * 
 	 * @author Joakim Candefors
-	  */
+	 */
 	public class DestinationButton extends JButton implements ActionListener{
 
 		private int actionType;
@@ -111,8 +112,6 @@ public class Slide {
 
 		/**
 		 * Constructor of a destination-button. This component takes the user to the destination referenced in this button. 
-		 * @param layoutHandler A LayoutHandler to lay out this component in the frame.
-		 * @param travelProject	A the name of the travelProject the destination belongs to.
 		 * @param destinationTitle	The title of the destination.
 		 * @param actionType
 		 */
@@ -130,39 +129,38 @@ public class Slide {
 		}
 		public void actionPerformed(ActionEvent e) {
 			switch (actionType){
-
 			case 1 : new Destination(layoutHandler, userName, super.getText(), false); 			//Open destination
 			break;
 			case 2 : showCreateDialog();														//create destination			
-			}
-
-			if(e.getActionCommand() == "Skapa"){
-				String input = destinationField.getText();
-				if(input.length() < 1 && input.length()>25){
-					JOptionPane.showMessageDialog(layoutHandler.getFrame(), "Destinationsnamnet måste vara minst 1 tecken och max 25 tecken.");						
-				}else if(destinations.contains(input)){
-					JOptionPane.showMessageDialog(layoutHandler.getFrame(), "Destinationsnamnet finns redan i reseprojektet.");
-				}else{
-					createDestination(input);
-				}					
-			}
+			}								
 		}
 
-		public void showCreateDialog(){
-			JDialog dialog = new JDialog(layoutHandler.getFrame());
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
+		/**
+		 * Shows a dialog after firing destination button. Asks user for title of the destination to be created. 
+		 */
+		public void showCreateDialog(){			
+			dialog = new JDialog(layoutHandler.getFrame());
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);			
+			dialog.setPreferredSize(new Dimension(300, 100));
 			dialog.setLocation(300, 300);
 			dialog.setAlwaysOnTop(true);
-			dialog.setFocusable(true);
-			dialog.setPreferredSize(new Dimension(290, 100));
-			dialog.setMaximumSize(new Dimension(300, 100));			//funkis?
+			dialog.setFocusable(true);			
+			dialog.setResizable(false);		
 			dialog.setBackground(Color.BLUE);
 			dialog.setLayout(new FlowLayout(FlowLayout.LEFT,2,2));
 			dialog.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
 			dialog.setTitle("Skapa Destination");
-			JTextField destinationField = new JTextField("destinationens namn");
+			destinationField = new JTextField("destinationens namn");
 			JButton createButton = new JButton("Skapa");
-			createButton.addActionListener(this);
+			createButton.addActionListener(new ActionListener() {				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					String input = checkInput();
+					if(input != null)
+						createDestination(input);					
+				}
+			});
 			dialog.add(new JLabel("Destinationens titel:"));
 			dialog.add(destinationField);
 			dialog.add(createButton);
@@ -170,14 +168,35 @@ public class Slide {
 			dialog.pack();
 		}	
 
+		/**
+		 * 
+		 */
+		private String checkInput(){
+			String input = destinationField.getText();
+			if(input.length() < 1 || input.length()>25 || input.matches("destinationens namn")){
+				JOptionPane.showMessageDialog(dialog, "Destinationsnamnet måste vara minst 1 tecken och max 25 tecken.");
+				return null;
+			}else if(destinations.contains(input)){
+				JOptionPane.showMessageDialog(dialog, "Destinationsnamnet finns redan i reseprojektet.");
+				return null;
+			}else{			
+				return input;
+			}
+		}
+		/**
+		 * Creates a new destination with the title set in the dialog in showCreateDialog().
+		 * @param destinationTitle the title of the new destination.
+		 */
 		private void createDestination(String destinationTitle){
 
 			new Destination(layoutHandler, userName, destinationTitle, true);	//Create destination
-			dialog.dispose();
-
+			dialog.dispose();													//close dialog
 		}
+
 	}
-
-
 }
+
+
+
+
 
